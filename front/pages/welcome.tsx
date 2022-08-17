a94593cbe4155b1eb98a7136d4ecfdea817be703
kaewkl_i <kaewkl_i@etna-alternance.net>
2022-08-17 18:47:31 +0200

    Add WelcomePage

--- front/pages/welcome.tsx
+++ front/pages/welcome.tsx
@@ -1,35 +1,55 @@
-import dynamic from "next/dynamic"
-import styled from "styled-components"
-import Form from "../components/logInForm";
+import styled from 'styled-components'
+import HomeInfo from '../components/HomeInfo'
+import Wrapper from '../components/Wrapper'
+import NextImage from 'next/image'
+import MotoImage from '../pictureAssets/BackGround/motoGp.png'
 
-const BackgroundImage = dynamic(() => import("../components/Wrapper"), {
-    ssr: false,
-  });
-  
-  const Wrapper = styled.div`
-    z-index: 1;
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-  `;
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
 
-  const Input = styled.div`
-    background-colo
-  `
-  
+const Header = styled.div`
+  height: 5rem;
+  margin-top: 3rem;
+  text-align: center;
+  color: white;
+  font-size: 2rem;
+  font-weight: bolder;
+`
+
+const HorizonLine = styled.hr`
+  width: 90%;
+  height: 2px;
+  background-color: white;
+  border: none;
+  padding-top: -10px;
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  justify-content: space-between;
+  padding: 70px;
+
+  padding-left: 15%;
+`
 
 export default function WelcomePage() {
-    return(
-    <>
-      <Wrapper>
-        <Form />
-      </Wrapper>
+  return (
+    <Wrapper>
+      <Container id="Conatiner">
+        <Header> W M C </Header>
+        <HorizonLine />
 
-      <BackgroundImage />
-      
-    </>
-  );
+        <Content id="Content">
+          <HomeInfo />
+          <NextImage src={MotoImage} />
+        </Content>
+      </Container>
+    </Wrapper>
+  )
 }
